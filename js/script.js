c8f6dcb54993e623d40352a199bc2236b173c987
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-11-04 10:39:13 +0100

    restored main contacs array /wo changes

--- js/script.js
+++ js/script.js
@@ -8,25 +8,19 @@ const app = Vue.createApp({
                     id: 1,
                     name: 'Michele',
                     avatar: '_1',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                             },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                             },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
                             status: 'received'
@@ -37,25 +31,19 @@ const app = Vue.createApp({
                     id: 2,
                         name: 'Fabio',
                     avatar: '_2',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
                             status: 'sent'
                             },
                         {
-                            menu: false,
-                            visible: true,
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                             },
                         {
-                            menu: false,
-                            visible: true,
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
@@ -66,25 +54,19 @@ const app = Vue.createApp({
                     id: 3,
                     name: 'Samuele',
                     avatar: '_3',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
                             status: 'received'
@@ -95,18 +77,14 @@ const app = Vue.createApp({
                     id: 4,
                     name: 'Alessandro B.',
                     avatar: '_4',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
@@ -117,18 +95,14 @@ const app = Vue.createApp({
                     id: 5,
                     name: 'Alessandro L.',
                     avatar: '_5',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
@@ -139,25 +113,19 @@ const app = Vue.createApp({
                     id: 6,
                     name: 'Claudia',
                     avatar: '_6',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
@@ -168,18 +136,14 @@ const app = Vue.createApp({
                     id: 7,
                     name: 'Federico',
                     avatar: '_7',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
@@ -190,25 +154,19 @@ const app = Vue.createApp({
                     id: 8,
                     name: 'Davide',
                     avatar: '_8',
-                    
+                    visible: true,
                     messages: [
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            menu: false,
-                            visible: true,
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
                             status: 'received'
@@ -216,6 +174,7 @@ const app = Vue.createApp({
                     ],
                     }
                     ],
+                    
             activeChatIndex: 0,
             searchValue: '',
             newMessageModel: {
@@ -226,7 +185,10 @@ const app = Vue.createApp({
             inputTextMessage: '',
             show: false,
             authorInfo: '',
-            dateInfo:''
+            dateInfo:'',
+            msgOpt: false,
+            menuIndex: null,
+            
             }
     },
     computed: {
@@ -319,6 +281,8 @@ const app = Vue.createApp({
 
         //take msg index and reset author and date, change boolean value of menu property
         menu(i){
+            this.menuIndex = i;
+            this.msgOpt = !this.msgOpt;
             this.authorInfo = '';
             this.dateInfo = '';
             this.contacts[this.activeChatIndex].messages[i].menu = !this.contacts[this.activeChatIndex].messages[i].menu
